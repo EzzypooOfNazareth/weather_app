d7a7e8601d69580f67a0fa7f5bdb13b76fa0b6c9
ezramoore <emoore13@ycp.edu>
2019-07-17 19:18:38 -0400

    updated css with animations and 404 page response

--- weather_app/src/weatherApp/views.py
+++ weather_app/src/weatherApp/views.py
@@ -10,14 +10,17 @@ def home_page(request):
     url = 'http://api.openweathermap.org/data/2.5/weather?q={}&units=imperial&appid=8e1196000bc1f959aa972f7744c8f591'
 
     if request.method == 'POST':
-        form = CityForm(request.POST)
-        city = form['name'].value()
-        r = requests.get(url.format(city)).json()
-        city_weather = {
-            'city' : city,
-            'temperature' : r['main']['temp'],
-            'description' : r['weather'][0]['description']
-        }
+        try:
+            form = CityForm(request.POST)
+            city = form['name'].value()
+            r = requests.get(url.format(city)).json()
+            city_weather = {
+                'city' : city,
+                'temperature' : r['main']['temp'],
+                'description' : r['weather'][0]['description']
+            }
+        except:
+            raise Http404
     else:
         city = 'Philadelphia'
         r = requests.get(url.format(city)).json()
